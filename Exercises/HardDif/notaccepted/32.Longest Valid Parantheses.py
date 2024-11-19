b90bca80a370be567db92142c8c2f0bbe8157575
tamernaimov <tamer.naimov@gmail.com>
2024-11-19 21:56:26 +0200

    tried doing a hard problem, but not good enough, try again tmrw

--- Exercises/HardDif/notaccepted/32.Longest Valid Parantheses.py
+++ Exercises/HardDif/notaccepted/32.Longest Valid Parantheses.py
@@ -1,23 +1,54 @@
-def longestValidParentheses(s):
-    longestPar = 0
-    i = 0
-    counter = 0
-    while i < len(s) - 1:
-        text = s
-        if text[i] + text[i + 1] == "()":
-            text.replace(text[i],  "")
-            text.replace(text[i+1],  "")
-            print(i)
-            counter += 2
-            i += 2
-        else:
-            counter = 0
-            i += 1
-            continue
-        if counter > longestPar:
-            longestPar = counter
 
+
+def longestValidParentheses(s):
+    sList = list(s)
+    longestPar = 0
+    counter = 0
+    theTruth = False
+    theTruth3 = True
+    while theTruth3:
+        if (len(sList) == 2 and sList[0] + sList[1] == "((" or
+            len(sList) == 2 and sList[0] + sList[1] == ")(" or
+            len(sList) == 2 and sList[0] + sList[1] == "))"):
+            if counter > longestPar:
+                longestPar = counter
+            break
+        i = 0
+
+        if sList[i] + sList[i+1] == "()":
+            del sList[i+1]
+            del sList[i]
+            print(sList)
+            counter += 2
+            if len(sList) < 2:
+                if counter > longestPar:
+                    longestPar = counter
+                break
+
+        else:
+            currentIndex = i
+            while i < len(sList)-2:
+                if sList[i+1] + sList[i+2] != "()" and sList[currentIndex] + sList[i+2] == "()":
+                    del sList[i+2]
+                    del sList[currentIndex]
+                    counter += 2
+                    theTruth = True
+                    break
+                i += 1
+            i = 0
+            if theTruth:
+                theTruth = False
+                continue
+            #ako ne sme ospeli da namerim:
+            if counter > longestPar:
+                longestPar = counter
+            i += 1
+            counter = 0
+
+        if i == len(s)-1:
+            break
+        if len(sList) < 2:
+            theTruth3 = False
     return longestPar
 
-
-print(longestValidParentheses("()(()()(()()"))
+print(longestValidParentheses("))(("))
