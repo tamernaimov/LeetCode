860564c2f95309b92e29ec1d50437407f90932d4
tamernaimov <tamer.naimov@gmail.com>
2024-12-27 20:32:07 +0200

    cleared up

--- Exercises/HardDif/notaccepted/41. First Missing Positive.py
+++
@@ -1,7 +0,0 @@
-def firstMissingPositive(nums):
-    for i in range (1,len(nums)+1):
-        if i not in nums:
-            return i
-    return max(nums)+1
-
-print(firstMissingPositive([1,1000]))
--- Exercises/test.py
+++
@@ -1,3 +0,0 @@
-test = "tamea"
-
-print(test.("a"))
